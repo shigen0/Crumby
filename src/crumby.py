8145ab8bbf3c7a1eeff81d082c86f2be8686f855
yaceno <75220653+yaceno@users.noreply.github.com>
2023-07-27 12:27:52 +0200

    Update crumby.py

--- src/crumby.py
+++ src/crumby.py
@@ -1,7 +1,9 @@
 import argparse
 from search import *
+from spray import *
 
-def crumby(host,auth,domain):
+def crumby(host,auth,domain,spray):
+    
     print("""
    _____                      _           
   / ____|                    | |          
@@ -12,17 +14,32 @@ def crumby(host,auth,domain):
                                      __/ |
                                     |___/                                       
     """)
-    # Get the number and values of dumped passwords
-    nbr_success,users_success = search_passwords(host,auth,domain)
+
+    # Get the complete list of users, users with a cracked account and information to connect (for spraying)
+    all_users,users_success,conn,server = search_passwords(host,auth,domain,spray)
 
     print("[*] Report")
-    print("[*] Number of dumped passwords : "+str(nbr_success))
-    print("[*] Accounts cracked : "+str(users_success))
+    print("[+] Number of dumped passwords : "+str(len(users_success)))
+    print("[+] Cracked accounts : "+str(users_success))
+
+    if spray:
+        print("\n[*] Spraying...")
+
+        # We subtract the list of users from the list of cracked accounts to iterate on those that have not been tested in order to carry out the spraying.
+        usernames_success = [success.split()[0] for success in users_success]
+        users_spray = [item for item in all_users if item not in usernames_success]
+
+        # We get the passwords that will be sprayed
+        passwords = [success.split()[1] for success in users_success]
+
+        accounts_cracked = spraying(passwords,users_spray,domain,server)
+        print("[+] Other accounts cracked with the passwords found : "+str(accounts_cracked))
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Password searching tool in object descriptions')
     parser.add_argument('-s', '--server', type=str, help='Server adress', required=True)
     parser.add_argument('-a', '--auth', type=str, help='Name and password (separated by :) of the account we want to connect to',required=True)
     parser.add_argument('-d', '--domain', type=str, help='Domain name',required=True)
+    parser.add_argument('-sp', '--spray', action="store_true", help='Spray the passwords found',required=False)
     args = parser.parse_args()
-    crumby(args.server,args.auth,args.domain)
+    crumby(args.server,args.auth,args.domain,args.spray)
